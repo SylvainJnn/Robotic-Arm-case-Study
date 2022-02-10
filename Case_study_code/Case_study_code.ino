03f7af76725d27720c75c760b5f20736a7ec1605
Sylvain Jannin <sylvainjannin@gmail.com>
2022-02-10 19:07:15 +0000

    First idea for the program, works for one servo

---
+++ Case_study_code/Case_study_code.ino
@@ -0,0 +1,104 @@
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = A0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+const int servo_pin = 2;
+const int LED = 1;
+const int Button_Calibration = 12; // ASK for Switch --> easier to implemant
+const int Button_Action = 13;
+
+int alpha_0;
+
+bool Read_button(int pin)           //return High if a button is pressed for two secods
+{
+  if(digitalRead(pin) == HIGH)      //check if the button is pressed
+    {
+      delay(2000);                  //wait 2 seconds
+      if(digitalRead(pin) == HIGH)  //check if it is still the case
+      {
+        return(HIGH);
+      }
+    }
+    return(LOW);
+}
+
+bool Read_Calibration_Button()
+{
+  if(digitalRead(Button_Calibration) == HIGH)
+    {
+      delay(2000);
+      if(digitalRead(Button_Calibration) == HIGH)
+      {
+        return(HIGH);
+      }
+    }
+    return(LOW);
+}
+
+void Calibration()
+{
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+ 
+  myservo.write(val);                  // sets the servo position according to the scaled value
+  delay(15);
+  
+}
+
+void Action() //for the moment, let's just check if it is working properly
+{
+  myservo.write(alpha_0 - 35);
+  delay(500);
+
+  myservo.write(alpha_0 - 45);
+  delay(500);
+
+  myservo.write(alpha_0 + 35);
+  delay(500);
+
+  myservo.write(alpha_0 +45);
+  delay(500);
+}
+
+
+
+void setup() 
+{
+  myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
+  
+  pinMode(Button_Calibration, INPUT);
+  pinMode(Button_Action, INPUT);
+  
+  pinMode(LED, OUTPUT);    
+}
+
+
+void loop() 
+{
+  bool Button_Calibration_state, Button_Calibration_action;
+  
+  Button_Calibration_state = Read_button(Button_Calibration);
+  //if else or while ? 
+  while(Button_Calibration_state)
+  {
+    digitalWrite(LED, HIGH);              //turn on LED to show that calibration is running
+    Calibration();
+    Button_Calibration_state = Read_button(Button_Calibration);
+
+    alpha_0 = myservo.read();
+    
+  }
+  digitalWrite(LED, LOW);                     //turn off LED to show that calibration is over
+  Button_Calibration_action = Read_button(Button_Action);
+
+  if(Button_Calibration_action)
+  {
+    Action();
+  }
+
+
+  delay(1000);                           // waits for the servo to get there
+}
