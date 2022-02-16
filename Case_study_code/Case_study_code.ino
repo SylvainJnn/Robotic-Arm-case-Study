23f60da57c0a05b9561c8080723d027e64938cef
Sylvain Jannin <sylvainjannin@gmail.com>
2022-02-16 00:23:39 +0000

    Change buttons to 1 switch, 3 servos and potentiometer, change the action

--- Case_study_code/Case_study_code.ino
+++ Case_study_code/Case_study_code.ino
@@ -1,16 +1,20 @@
 #include <Servo.h>
 
-Servo myservo;  // create servo object to control a servo
+Servo servo_alpha, servo_beta, servo_gama; // create servo object to control a servo
 
-int potpin = A0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+const int potpin_alpha = A0;  // analog pin used to connect the potentiometer
+const int potpin_beta  = A1;  // analog pin used to connect the potentiometer
+const int potpin_gama  = A2;  // analog pin used to connect the potentiometer
+                   
+const int servo_pin_alpha = 2;
+const int servo_pin_beta  = 3;
+const int servo_pin_gama  = 4;
 
-const int servo_pin = 2;
 const int LED = 1;
 const int Button_Calibration = 12; // ASK for Switch --> easier to implemant
 const int Button_Action = 13;
 
-int alpha_0;
+int alpha_0, beta_0, gama_0; // Home angle positions
 
 bool Read_button(int pin)           //return High if a button is pressed for two secods
 {
@@ -27,26 +31,49 @@ bool Read_button(int pin)           //return High if a button is pressed for two
 
 void Calibration()
 {
-  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+  int new_alpha_0, new_beta_0, new_gama_0;
+  //Read Values
+  new_alpha_0 = analogRead(potpin_alpha);             // reads the value of the potentiometer (value between 0 and 1023)
+  new_alpha_0 = map(new_alpha_0, 0, 1023, 0, 180);    // scale it to use it with the servo (value between 0 and 180)
+
+  new_beta_0  = analogRead(potpin_beta);              // reads the value of the potentiometer (value between 0 and 1023)
+  new_beta_0  = map(new_beta_0, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
+
+  new_gama_0  = analogRead(potpin_gama);              // reads the value of the potentiometer (value between 0 and 1023)
+  new_gama_0  = map(new_gama_0, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
  
-  myservo.write(val);                  // sets the servo position according to the scaled value
+  //Write Values
+  servo_alpha.write(new_alpha_0);                  // sets the servo position according to the scaled value
+  alpha_0 = servo_alpha.read();
+
+  servo_beta.write(new_beta_0);                  // sets the servo position according to the scaled value
+  beta_0 = servo_beta.read();
+  
+  servo_gama.write(new_gama_0);                  // sets the servo position according to the scaled value
+  gama_0 = servo_gama.read();
+  
   delay(15);
   
 }
 
 void Action() //for the moment, let's just check if it is working properly
 {
-  myservo.write(alpha_0 - 35);
+  servo_alpha.write(alpha_0 - 35);
   delay(500);
 
-  myservo.write(alpha_0 - 45);
+  servo_alpha.write(alpha_0 - 35);
   delay(500);
 
-  myservo.write(alpha_0 + 35);
+  servo_alpha.write(alpha_0 + 55);
+  servo_beta.write(beta_0 + 20);
+  delay(500);
+  
+  servo_alpha.write(alpha_0 - 55);
+  servo_beta.write(beta_0 - 20);
+  servo_gama.write(gama_0 + 45);
   delay(500);
 
-  myservo.write(alpha_0 +45);
+  servo_gama.write(gama_0 - 45);
   delay(500);
 }
 
@@ -54,7 +81,10 @@ void Action() //for the moment, let's just check if it is working properly
 
 void setup() 
 {
-  myservo.attach(servo_pin);  // attaches the servo on pin 9 to the servo object
+  // Set Servos
+  servo_alpha.attach(potpin_alpha);  
+  servo_beta.attach(potpin_beta);  
+  servo_gama.attach(potpin_gama); 
   
   pinMode(Button_Calibration, INPUT);
   pinMode(Button_Action, INPUT);
@@ -65,28 +95,18 @@ void setup()
 
 void loop() 
 {
-  bool Button_Calibration_state, Button_Calibration_action;
-  
-  Button_Calibration_state = Read_button(Button_Calibration);
-  //if else or while ? 
-  while(Button_Calibration_state)         //Pressed the button for 2.5seconds -> calibration starts. Repressed this button to end the loop
+
+  if(digitalRead(Button_Calibration) == HIGH)
   {
-    Button_Calibration_state = LOW;       //Once the button is pressed we need to set it low. The idea is: we calibrate if user press the button, if he repressed the button it means the calibration is over. So once we enter the calibration we set ti to low, we re read the button at the end of the while loops to see if the calibration continues or not.
+    Calibration();  
     digitalWrite(LED, HIGH);              //turn on LED to show that calibration is running
-    Calibration();
-    Button_Calibration_state = Read_button(Button_Calibration);
-
-    alpha_0 = myservo.read();
-    
+      
   }
-  digitalWrite(LED, LOW);                     //turn off LED to show that calibration is over
-  Button_Calibration_action = Read_button(Button_Action);
-
-  if(Button_Calibration_action)
-  {
+  else
+  {    
+    digitalWrite(LED, LOW);              //turn on LED to show that calibration is running
     Action();
   }
-
 
   delay(1000);                           // waits for the servo to get there
 }
