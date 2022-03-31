11b5a5b82a499979984b74984f457d222201e7ba
Sylvain Jannin <sylvainjannin@gmail.com>
2022-03-31 13:57:50 +0100

    add gripper

--- Case_study_code/Case_study_code.ino
+++ Case_study_code/Case_study_code.ino
@@ -4,7 +4,7 @@
 
 
 // ============= Program variables =============
-Servo servo_q1, servo_q2, servo_q3, servo_q4; // create servo object to control a servo
+Servo servo_q1, servo_q2, servo_q3, servo_q4, servo_gripper; // create servo object to control a servo
 const int potpin_q1 = A0;  // analog pin used to connect the potentiometer
 const int potpin_q2  = A1;  // analog pin used to connect the potentiometer
 const int potpin_q3  = A2;  // analog pin used to connect the potentiometer
@@ -13,6 +13,7 @@ const int servo_pin_q1  = 3;
 const int servo_pin_q2  = 5;
 const int servo_pin_q3  = 6;
 const int servo_pin_q4  = 9;
+const int servo_pin_gripper = 10;
 
 const int LED = 11;
 const int Button_Calibration = 12; // ASK for Switch --> easier to implemant
@@ -69,12 +70,7 @@ void Calibration()
 
 
 // ===================================== action & poses =====================================
-void pose_home()// home position
-{
-  servo_q1.write(q1_0);
-  servo_q2.write(q2_0);
-  servo_q3.write(q3_0);
-}
+
 
 float compute_phi(float x)//comoute phi angle for the end effector
 {
@@ -121,25 +117,25 @@ float compute_q2(float r,float z,float l1,float l2, float q3)
 //make the servos move by writing the angle
 void action_equation(float q1, float q2, float q3, float q4)
 {
-  
+  /*
   Serial.println("angles");
   Serial.println(q1);
   Serial.println(q2);
   Serial.println(q3);
   Serial.println(q4);
-  
+  */
   
   //Serial.println("q1");
   servo_q1.write(q1 );
+  delay(1000);
+  //Serial.println("q4");
+  servo_q4.write(q4 );
   delay(1000);
   //Serial.println("q2");
   servo_q2.write(q2 );
   delay(1000);
   //Serial.println("q3");
   servo_q3.write(q3 );
-  delay(1000);
-  //Serial.println("q4");
-  servo_q4.write(q4 );
   delay(1000);
 }
 void action_equation2(float q1, float q2, float q3, float q4)// write
@@ -181,7 +177,7 @@ void pose(float *direct_model)//takes an array
   float z = direct_model[2] - d;//take off the d from DH parameter
   float phi = compute_phi(x); //in our case phi is always 0
 
-  float r =(float) sqrt(sq(x) + sq(y)); //I don't know how to decreibe it yes baisicly, in this on this is line that the planer is based
+  float r =(float) sqrt(sq(x) + sq(y)); //we take the hypotenus because except the first servo, all the other servo works in the plan plan
 
   q1 = atan2f(y,x); // comoute q1, it give the direction so that the other servo works only on the same plan
   
@@ -189,12 +185,13 @@ void pose(float *direct_model)//takes an array
   float r3, z3;
   r3 = compute_r3(r, l3, phi);//this is position of servo4
   z3 = compute_z3(z, l3, phi);
+  
   //compute q
   q3  = compute_q3(r3, z3, l1, l2);
   q2  = compute_q2(r3, z3, l1, l2, q3);
   q4 = phi - (q2+q3);
 
-  
+  //add offsets
   if(q2 <0)
     q2 = -q2; // the angles is negative in our case, but we want it positive ( it is negative based on the trigonometrique cercle) because we make the servo move on the opposite sens
 
@@ -219,19 +216,96 @@ void pose(float *direct_model)//takes an array
 
 
 
-// Gripper
+// Gripper - picking and dropping 
 void pick()
 {
-  //nothing for the moment, basicly we want to set the other servos to grip somthing
+  Serial.println("picking");
+  servo_gripper.write(180);// We don't know fot the moment which one of these two is the good one 
 }
 
 void drop()
 {
-  //nothing for the moment, set the servos to drop
+  Serial.println("Dropping");
+  servo_gripper.write(10);
 }
 
 
+void home_pose()
+{
+  Serial.println("home_pose");
+  float directmodel[3];
+  directmodel[0] = (l2 + l3)*0.95;
+  directmodel[1] = 0;//(l1+l3+l2)*0.95;
+  directmodel[2] = d+l1;//d + l1;
 
+  drop();
+  pose(directmodel);
+  
+}
+
+void pose_1()//ready to pick
+{
+  float directmodel[3];
+  Serial.println("pose 1: move and pick");
+  directmodel[0] = l1+l3 ;
+  directmodel[1] = 0;
+  directmodel[2] = d-l2;
+  
+  //drop();
+  pose(directmodel);
+  pick();
+  delay(5000);
+  
+}
+
+void pose_2()//took somthing and go up
+{
+  float directmodel[3];
+  Serial.println("pose 2: go up");
+  directmodel[0] = (l1+l3+l2)*0.95;
+  directmodel[1] = 0;
+  directmodel[2] = d;
+
+  pick();
+  pose(directmodel);
+  pick();
+  delay(5000);
+}
+
+void pose_3()//move
+{
+  float directmodel[3];
+  Serial.println("pose 3: Rotate");
+  directmodel[0] = 0;
+  directmodel[1] = (l2 + l3)*0.95;
+  directmodel[2] = d+l1;//d + l1;
+
+  pose(directmodel);
+  delay(5000);
+}
+
+void pose_4()// move and ready to drop
+{
+  float directmodel[3];
+  Serial.println("pose 4: drop object");
+  directmodel[0] =  0;
+  directmodel[1] = l1+l3;
+  directmodel[2] = d-l2;
+  
+  pose(directmodel);
+  drop();
+  delay(5000);
+}
+
+void Action()
+{
+  home_pose();
+  pose_1();
+  pose_2();
+  pose_3();
+  pose_4();
+  home_pose();
+}
 
 
 void setup() 
@@ -241,12 +315,14 @@ void setup()
   servo_q1.attach(servo_pin_q1);  
   servo_q2.attach(servo_pin_q2);  
   servo_q3.attach(servo_pin_q3); 
-  servo_q4.attach(servo_pin_q4); 
+  servo_q4.attach(servo_pin_q4);
+  servo_gripper.attach(servo_pin_gripper); 
   
   pinMode(servo_pin_q1, OUTPUT);
   pinMode(servo_pin_q2, OUTPUT);
   pinMode(servo_pin_q3, OUTPUT);
   pinMode(servo_pin_q4, OUTPUT);
+  pinMode(servo_pin_gripper, OUTPUT);
   
   pinMode(Button_Calibration, INPUT);
   pinMode(Button_Action, INPUT);
@@ -257,7 +333,7 @@ void setup()
 
 void loop() 
 {
-
+/*
     float directmodel[3];//set a array of 3 for the direct model (x,y,z)
     //save home position (init pose)
     delay(500); 
@@ -268,6 +344,25 @@ void loop()
 
     
     delay(500); 
+*/
+    
+    
+    
+    /*
+    while(1)
+    {
+    //action();
+    //delay(10000);
+    Serial.println("yo");
+    pick();
+    delay(2000);
+    drop();
+    delay(2000);
+    }
+    */
+
+    Action();
+    /*
     while(1)
     {
       Serial.println("POSE 1");
@@ -301,5 +396,5 @@ void loop()
       directmodel[2] = d-l2;
       pose(directmodel);
       delay(5000); 
-    }
+    }*/
 }
